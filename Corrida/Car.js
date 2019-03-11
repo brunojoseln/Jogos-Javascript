fb0379ccc40bae50a2a160c4daff4b1ce2e8b454
Bruno José <bruno.lima184@etec.sp.gov.br>
2019-03-11 00:28:42 -0300

    Update Car.js

--- Corrida/Car.js
+++ Corrida/Car.js
@@ -53,3 +53,45 @@ function carDraw(){
     drawBitmapCenteredWithRotation(carPic, carX, carY, carAng);
 }
 
+
+
+
+
+
+//imageLoading
+
+var carPic = document.createElement("img");
+var roadPic = document.createElement("img");
+var wallPic = document.createElement("img");
+
+var picsToLoad = 0; //ser automatically based on imageList in loadImages()
+
+function countLoadedImagesAndLaunchIfReady(){
+    picsToLoad--;
+    console.log(picsToLoad);
+    if (picsToLoad == 0){
+        imageLoadingDone();
+    }
+}
+
+function beginLoadingImage(imgVar,fileName){
+    imgVar.onload = countLoadedImagesAndLaunchIfReady;
+    imgVar.src = fileName;
+}
+
+function loadImages(){
+    var dataSet = {varName: carPic, theFile: "carro.png"};
+
+    var imageList = [
+        {varName: carPic, theFile: "https://raw.githubusercontent.com/brunojoseln/Jogos-Javascript/master/Corrida/carro.png"},
+        {varName: roadPic, theFile: "https://raw.githubusercontent.com/brunojoseln/Jogos-Javascript/master/Corrida/track_road.png"},
+        {varName: wallPic, theFile: "https://raw.githubusercontent.com/brunojoseln/Jogos-Javascript/master/Corrida/track_wall.png"}
+    ];
+
+
+    picsToLoad = imageList.length;
+
+    for(var i=0; i< imageList.length; i++){
+        beginLoadingImage(imageList[i].varName, imageList[i].theFile);
+    }
+}
